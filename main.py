65a92c6636a77de04e18cf1c3458476e3086e211
Levi Scott <121728481+LeviScott29@users.noreply.github.com>
2024-01-18 17:53:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# strips only names from the text document and puts them in a list
 with open ("names.txt") as names_list:
     names = names_list.readlines()
     print(names)
@@ -6,7 +7,8 @@ with open ("names.txt") as names_list:
         split_group = name.split()
         the_names = split_group[1].strip()
         only_names.append(the_names)
-
+        
+#takes invite template and makes a new document replacing the name block for each person on the names list
     for replacement in only_names:
         file_name= f"letter to_{replacement}"
         with open("starting_letter.txt", "r") as file:
